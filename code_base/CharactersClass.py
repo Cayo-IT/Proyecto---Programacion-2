ee6db8e6cb42226ddff76242efcffe02dd4c061d
Dekariuz <dekariuz@gmail.com>
2026-04-24 00:48:42 +0000

    creacion de carpetas 1

---
+++ code_base/CharactersClass.py
@@ -0,0 +1,8 @@
+class Player:
+""" Caracteristicas del jugador"""
+class enemies:
+""" Caracteristicas del enemigo"""
+class colision:
+""" Coliciones de las balas , enemigos y jugador """
+class objets:
+""" Balas, powerups , score, vidas.""""
---
+++ code_base/game_manager.py
@@ -0,0 +1,5 @@
+import pygame
+
+pygame.init()
+alto, ancho = 1280, 720
+screen = pygame.screen(alto, ancho)
